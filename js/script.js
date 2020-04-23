7b947b57a1686d5fbaab53eea9f3a8adff367dd0
nahim <nahim.felicito@gmail.com>
2020-04-23 15:58:20 -0300

    bug fix

--- js/script.js
+++ js/script.js
@@ -14,12 +14,11 @@ var namePlayer2 = "";
 function setName(id){
   if(id == "player1"){
     namePlayer1 = document.getElementById(id).value;
-    console.log("setnames1")
   } 
   if(id == "player2"){
     namePlayer2 = document.getElementById(id).value;
-    console.log("setnames2")
   }
+  document.getElementById("whoPlay").innerHTML = "Jugador " + namePlayer1 +" ("+ turn + ") es su turno de jugar!";
 }
 
 function choosePlayers(value){
@@ -30,33 +29,40 @@ function choosePlayers(value){
     player2 = "x";
   }
   turn = player1;
+  document.getElementById("whoPlay").innerHTML = "Jugador " + namePlayer1 +" ("+ turn + ") es su turno de jugar!";
 }
 
 function markXorO(id, row, column){
-  if(document.getElementById(id).innerHTML != "x" && document.getElementById(id).innerHTML != "o"){
-    playRounds++; //counter 
-    if(turn == player1){
-      document.getElementById(id).innerHTML = player1 ;
-      board[row][column]= player1; //mark x or o in board
-    } else {
-      document.getElementById(id).innerHTML = player2 ;
-      board[row][column]= player2; //mark x or o in board
-    }
-    
-    if (playRounds > 4) { //first chance to win is at the 5th turn when the first player has played 3 times
-      checkWinner();
-    }
-    changeTurn();
-} else{
-    alert("Casillero ocupado, por favor seleccione otro casillero");
+  if(winner == ""){ //check if there is a winner, if not mark x or o 
+    if(document.getElementById(id).innerHTML != "x" && document.getElementById(id).innerHTML != "o"){
+      playRounds++; //counter 
+      if(turn == player1){
+        document.getElementById(id).innerHTML = player1 ;
+        board[row][column]= player1; //mark x or o in board
+      } else {
+        document.getElementById(id).innerHTML = player2 ;
+        board[row][column]= player2; //mark x or o in board
+      }
+      
+      if (playRounds > 4) { //first chance to win is at the 5th turn when the first player has played 3 times
+        checkWinner();
+      }
+      changeTurn();
+  } else{
+      alert("Casillero ocupado, por favor seleccione otro casillero");
+  }
+} else {
+  alert("El juego ha terminado, por favor comience un nuevo juego");
 }
 }
 
 function changeTurn(){
     if(turn == player1){
     turn = player2;
+    document.getElementById("whoPlay").innerHTML = "Jugador " + namePlayer2 +" ("+ turn + ") es su turno de jugar!";
   } else {
     turn = player1; 
+    document.getElementById("whoPlay").innerHTML = "Jugador " + namePlayer1 +" ("+ turn + ") es su turno de jugar!";
   }
 }
 
@@ -97,11 +103,9 @@ function printWinner(){
   if(turn == player1){
     console.log(namePlayer1)
     winner = namePlayer1;
-    console.log("entre a 1 " + winner + ", nameplayer1 "  + namePlayer1);
   } else {
     console.log(namePlayer2)
     winner = namePlayer2;
-    console.log("entre a 2 " + winner + ", nameplayer2 "  + namePlayer2);
   }
   document.getElementById("gameResult").innerHTML = winner + " ha ganado!";
 
@@ -117,8 +121,10 @@ function newGame(){
     document.getElementById(j).innerHTML = "";
   }
   document.getElementById("gameResult").innerHTML = "";
+  document.getElementById("whoPlay").innerHTML = "";
   playRounds = 0;
   winner = "";
+  turn=player1;
 }
 //juego no puede empezar si no hay nombres
 //no poder seguir poniendo x o o al finalizar 
